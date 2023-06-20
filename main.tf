9f18ee9ce2e28f5e4ba03e26efb751e11313a989
sofiaponteb <aaponte@unal.edu.co>
2023-06-20 10:12:29 -0500

    hosts 16x64 RHEL 8

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "ibm_compute_vm_instance" "virtual-server-classic-control-plane" {
   count                      = local.server_count_control_plane
   hostname                   = "control-plane-virtual-server-${count.index + 1}"
   domain                     = "IBM-PoC-Landing-Zone-Enterprise.cloud"
-  os_reference_code          = "REDHAT_7_64"
+  os_reference_code          = "REDHAT_8_64"
   datacenter                 = "dal13"
   network_speed              = 10
   hourly_billing             = false
@@ -28,13 +28,13 @@ resource "ibm_compute_vm_instance" "virtual-server-classic-host" {
   count                      = local.server_count_host
   hostname                   = "host-virtual-server-${count.index + 1}"
   domain                     = "IBM-PoC-Landing-Zone-Enterprise.cloud"
-  os_reference_code          = "REDHAT_7_64"
+  os_reference_code          = "REDHAT_8_64"
   datacenter                 = "dal13"
   network_speed              = 10
   hourly_billing             = false
   private_network_only       = false
-  cores                      = 4
-  memory                     = 16384
+  cores                      = 16
+  memory                     = 65536
   disks                      = [25]
   user_metadata              = "{\"value\":\"newvalue\"}"
   dedicated_acct_host_only   = true
